fdf9e1b56c9178892571513ee7bfe23a25e3d227
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2023-08-13 07:20:50 +0000

    Reading a user provided integer as input for the Fibonacci function

--- code/src/main.c
+++ code/src/main.c
@@ -4,9 +4,12 @@
 #include "fibonacci.h"
 
 int main() {
-  int input = -3;
-  int result = fibonacci(input);
+  printf("insert an integer n to get the nth Fibonacci number: ");
 
+  int input;
+  scanf("%d", &input);
+
+  int result = fibonacci(input);
   if (result < 0) return EXIT_FAILURE;
 
   printf("the #%d Fibonacci number is the number %d.\n", input, result);
