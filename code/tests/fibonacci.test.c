d0601efba5b744f91c1217076934def9a9bf288e
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2023-12-03 01:28:52 +0000

    Making first test file as example (we still need to figure out how to build and execute tests for ThrowTheSwitch/Unity)

---
+++ code/tests/fibonacci.test.c
@@ -0,0 +1,23 @@
+#include "../deps/Unity-2.5.2/src/unity.h"
+#include "../src/fibonacci.h"
+
+void setUp(void) {}
+
+void tearDown(void) {}
+
+void test_function_should_return_one(void) {
+  // Arrange
+  int input1 = 0; int input2 = 1;
+  // Act
+  int result1 = fibonacci(input1);
+  int result2 = fibonacci(input2);
+  // Assert
+  TEST_ASSERT_EQUAL_INT(1, result1);
+  TEST_ASSERT_EQUAL_INT(1, result2);
+}
+
+int main(void) {
+  UNITY_BEGIN();
+  RUN_TEST(test_function_should_return_one);
+  return UNITY_END();
+}
