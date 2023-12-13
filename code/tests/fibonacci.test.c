f34776d3c3bfcf5ffc3850f894a8856c5fce17ed
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2023-12-13 04:30:02 +0000

    Adding new test cases to match the Fibonacci function expected behavior

--- code/tests/fibonacci.test.c
+++ code/tests/fibonacci.test.c
@@ -16,8 +16,34 @@ void it_should_return_one(void) {
   TEST_ASSERT_EQUAL_INT(1, result2);
 }
 
+void it_should_compute_fibonacci_correctly(void) {
+  // Arrange
+  int input1 = 2; int input2 = 5; int input3 = 9; int input4 = 13;
+  // Act
+  int result1 = fibonacci(input1);
+  int result2 = fibonacci(input2);
+  int result3 = fibonacci(input3);
+  int result4 = fibonacci(input4);
+  // Assert
+  TEST_ASSERT_EQUAL_INT(2, result1);
+  TEST_ASSERT_EQUAL_INT(8, result2);
+  TEST_ASSERT_EQUAL_INT(55, result3);
+  TEST_ASSERT_EQUAL_INT(377, result4);
+}
+
+void it_should_not_accept_negative_input(void) {
+  // Arrange
+  int input1 = -5;
+  // Act
+  int result1 = fibonacci(input1);
+  // Assert
+  TEST_ASSERT_EQUAL_INT(-1, result1);
+}
+
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(it_should_return_one);
+  RUN_TEST(it_should_compute_fibonacci_correctly);
+  RUN_TEST(it_should_not_accept_negative_input);
   return UNITY_END();
 }
