3948d592299a831c5214361e51b39c9bc86fad06
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2024-04-20 02:49:31 -0300

    Testing both recursion and memoization versions of Fibonacci

--- code/tests/fibonacci.test.c
+++ code/tests/fibonacci.test.c
@@ -5,7 +5,42 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
-void it_should_return_one(void) {
+void rec_should_return_one(void) {
+  // Arrange
+  int input1 = 0; int input2 = 1;
+  // Act
+  int result1 = fibonacci_rec(input1);
+  int result2 = fibonacci_rec(input2);
+  // Assert
+  TEST_ASSERT_EQUAL_INT(1, result1);
+  TEST_ASSERT_EQUAL_INT(1, result2);
+}
+
+void rec_should_compute_fibonacci_correctly(void) {
+  // Arrange
+  int input1 = 2; int input2 = 5; int input3 = 9; int input4 = 13;
+  // Act
+  int result1 = fibonacci_rec(input1);
+  int result2 = fibonacci_rec(input2);
+  int result3 = fibonacci_rec(input3);
+  int result4 = fibonacci_rec(input4);
+  // Assert
+  TEST_ASSERT_EQUAL_INT(2, result1);
+  TEST_ASSERT_EQUAL_INT(8, result2);
+  TEST_ASSERT_EQUAL_INT(55, result3);
+  TEST_ASSERT_EQUAL_INT(377, result4);
+}
+
+void rec_should_not_accept_negative_input(void) {
+  // Arrange
+  int input1 = -5;
+  // Act
+  int result1 = fibonacci_rec(input1);
+  // Assert
+  TEST_ASSERT_EQUAL_INT(INVALID_INPUT_ERRNO, result1);
+}
+
+void memo_should_return_one(void) {
   // Arrange
   int input1 = 0; int input2 = 1;
   // Act
@@ -16,7 +51,7 @@ void it_should_return_one(void) {
   TEST_ASSERT_EQUAL_INT(1, result2);
 }
 
-void it_should_compute_fibonacci_correctly(void) {
+void memo_should_compute_fibonacci_correctly(void) {
   // Arrange
   int input1 = 2; int input2 = 5; int input3 = 9; int input4 = 13;
   // Act
@@ -31,7 +66,7 @@ void it_should_compute_fibonacci_correctly(void) {
   TEST_ASSERT_EQUAL_INT(377, result4);
 }
 
-void it_should_not_accept_negative_input(void) {
+void memo_should_not_accept_negative_input(void) {
   // Arrange
   int input1 = -5;
   // Act
@@ -42,8 +77,11 @@ void it_should_not_accept_negative_input(void) {
 
 int main(void) {
   UNITY_BEGIN();
-  RUN_TEST(it_should_return_one);
-  RUN_TEST(it_should_compute_fibonacci_correctly);
-  RUN_TEST(it_should_not_accept_negative_input);
+  RUN_TEST(rec_should_return_one);
+  RUN_TEST(rec_should_compute_fibonacci_correctly);
+  RUN_TEST(rec_should_not_accept_negative_input);
+  RUN_TEST(memo_should_return_one);
+  RUN_TEST(memo_should_compute_fibonacci_correctly);
+  RUN_TEST(memo_should_not_accept_negative_input);
   return UNITY_END();
 }
