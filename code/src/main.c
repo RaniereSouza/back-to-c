8f4201e8fce3f8fe75b98277f7f3d164045162a1
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2024-04-24 03:39:36 -0300

    Adding Raylib 5 as new dependency; Rewriting main.c to test opening a window with text using Raylib; Special targets to deal with Raylib and custom compilation for it (compiled Raylib to OpenGL 1.1 to be compatible with WSL on my personal machine, which is Ubuntu 20.04.6 LTS and graphic card Intel Iris Xe)

--- code/src/main.c
+++ code/src/main.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "fibonacci.h"
+// #include "fibonacci.h"
+//
+// int main() {
+//   printf("insert an integer n to get the nth Fibonacci number: ");
+//
+//   int input;
+//   scanf("%d", &input);
+//
+//   int result = fibonacci_memo(input);
+//   if (result == INVALID_INPUT_ERRNO) return EXIT_FAILURE;
+//
+//   printf("the #%d Fibonacci number is the number %d.\n", input, result);
+//   return EXIT_SUCCESS;
+// }
 
-int main() {
-  printf("insert an integer n to get the nth Fibonacci number: ");
+#include "../deps/raylib_5/src/raylib.h"
 
-  int input;
-  scanf("%d", &input);
+int main(void) {
+  InitWindow(800, 450, "First successful window created!");
 
-  int result = fibonacci_memo(input);
-  if (result == INVALID_INPUT_ERRNO) return EXIT_FAILURE;
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+      ClearBackground(RAYWHITE);
+      DrawText("Hello, Raylib 5!", 320, 200, 20, GRAY);
+    EndDrawing();
+  }
 
-  printf("the #%d Fibonacci number is the number %d.\n", input, result);
-  return EXIT_SUCCESS;
+  CloseWindow();
+
+  return 0;
 }
